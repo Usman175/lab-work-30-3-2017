1d28fddb924eb0b0a8417520bad37d26af022532
Usman175 <usmanali175@yahoo.com>
2017-03-30 04:49:04 -0700

    labwork

---
+++ file/app.js
@@ -0,0 +1,50 @@
+// var arr = ["Apple","Orange","Graps"];
+// //Insert a string in array through Push Method
+// document.write("<b>Push</b></br>");
+// document.write(arr);
+// arr.push("Banana");
+// document.write("<br>"+arr);
+
+// //delete a string in array through pop Method
+// document.write("<br><b>Pop</b></br>");
+// document.write(arr);
+// arr.pop();
+// document.write("<br>"+arr);
+
+// //delete a string in array through shift Method
+// document.write("<br><b>Shift</b></br>");
+// document.write(arr);
+// arr.shift();
+// document.write("<br>"+arr);
+
+// //Insert a string in array through unshift Method
+// document.write("<br><b>Unshift</b></br>");
+// document.write(arr);
+// arr.unshift("apple");
+// document.write("<br>"+arr);
+
+// document.write("SPLICE");
+// var arr = ["Apple","Orange","Graps"];
+//  document.write("<br>"+arr);
+// arr.splice(1);
+//  document.write("<br>"+arr);
+// arr.splice(1,0,"peach");
+//  document.write("<br>"+arr);
+
+
+// document.write("<br><br> SLICE");
+// var arr = ["Apple","Orange","Graps"];
+//  document.write("<br>"+arr);
+// var arr2 = arr.slice(1);
+//  document.write("<br>"+arr2);
+// var arr2 = arr.slice(0,2);
+//  document.write("<br>"+arr2);
+
+
+
+
+for(var a=1 ;  a<=10 ; a++){
+
+document.write("<br>2 x "+a+" = "+ a*2);
+
+}
